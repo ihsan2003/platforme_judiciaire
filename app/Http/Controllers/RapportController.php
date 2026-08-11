53c9bc06b84d430e4f657b606551a1423f534943
ihsan2003 <ihsanennaji15@gmail.com>
2026-08-11 13:20:31 +0100

    32

--- app/Http/Controllers/RapportController.php
+++ app/Http/Controllers/RapportController.php
@@ -18,37 +18,6 @@ class RapportController extends Controller
     }
 
     /**
-     * Aperçu rapide (AJAX) des principaux indicateurs pour la période
-     * sélectionnée, affiché dans le formulaire avant de générer le
-     * document Word complet.
-     */
-    public function apercu(Request $request)
-    {
-        $request->validate([
-            'date_debut' => ['required', 'date'],
-            'date_fin'   => ['required', 'date', 'after_or_equal:date_debut'],
-        ]);
-
-        $debut = Carbon::parse($request->input('date_debut'));
-        $fin = Carbon::parse($request->input('date_fin'));
-
-        $service = new RapportStatistiqueService($debut, $fin);
-        $valeurs = $service->genererStatistiques();
-
-        return response()->json([
-            'dossiers_total'    => $valeurs['dossiers_total'] ?? '0',
-            'dossiers_en_cours' => $valeurs['dossiers_en_cours'] ?? '0',
-            'dossiers_juges'    => $valeurs['dossiers_juges'] ?? '0',
-            'dossiers_executes' => $valeurs['dossiers_executes'] ?? '0',
-            'recl_total'        => $valeurs['recl_total'] ?? '0',
-            'recl_traitees'     => $valeurs['recl_traitees'] ?? '0',
-            'montant_total'     => $valeurs['montant_total'] ?? '0',
-            'nb_regions'        => count($service->lignesRegion()),
-            'nb_types_affaire'  => count($service->lignesTypeAffaire()),
-        ]);
-    }
-
-    /**
      * Génère le rapport Word rempli pour la période demandée et le renvoie
      * en téléchargement.
      */
